37a4382efd1676458475fc87e55d6ea8d2a206ae
Noah19cyber <alineborgescunha.abc@gmail.com>
2024-08-16 20:02:08 -0300

    PaM

---
+++ PAMNotes/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace PAMNotes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string filePath = Path.Combine(FileSystem.AppDataDirectory, "Notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists(filePath))
+            {
+                NoteEditor.Text = File.ReadAllText(filePath);
+            }
+        }
+
+        private void SaveBtn_Clicked(object sender, EventArgs e)
+        {
+            string text = NoteEditor.Text;
+
+            File.WriteAllText(filePath,
+                text);
+            DisplayAlert("Arquivo salvo");
+
+        }
+
+        private void DeleteBtn_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                DisplayAlert("Alerta", "Arquivo foi deletado");
+
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Arquivo não encontrado", "Ok");
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+"#Fa‡a a descri‡Æo do conte£do do seu projeto aqui" 
+"Projeto PaM aula de mobile" 
